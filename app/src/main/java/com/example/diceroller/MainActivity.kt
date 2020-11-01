6d2376e948af94fb92c0f3d2d1b0653bc7f8d508
carolinamaciel1 <carolinamaciel37@gmail.com>
2020-11-01 14:42:33 -0300

    initial commits

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+//            Toast.makeText(this, "button clicked.", Toast.LENGTH_LONG).show()
+            rollDice()
+        }
+
+
+    }
+
+    private fun rollDice() {
+        val textResult : TextView = findViewById(R.id.result_text)
+        val radomInt = Random().nextInt(6) + 1
+        textResult.text = radomInt.toString()
+    }
+}
